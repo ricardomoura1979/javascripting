5f374f4a1f3f09107b94ca3cfdb8725e691d5473
Ricardo Moura <ricardomoura1979@gmail.com>
2022-04-18 15:24:40 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,6 @@
+const pets = ['cat', 'dog', 'rat']
+  for pets[0, 1, 2] = pets[0, 1, 2] + 's'
+console.log(pets)
+
+
+// tirar dúvida com professor acerca deste problema
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+let pizza = 'pizza is alright'
+pizza.replace('alrigh' , 'wonderful')
+console.log(pizza)
